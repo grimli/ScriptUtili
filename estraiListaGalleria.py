6557aaabe689829e8e588540474d060c449593f1
Edoardo Bompiani <grimli_clex@yahoo.it>
2015-04-10 13:00:41 +0200

    some fix

--- estraiListaGalleria.py
+++ estraiListaGalleria.py
@@ -5,9 +5,9 @@ import gzip
 import re
 
 
-fileListaBlog="lista_blog.txt"
-listaLog='../20150317_blog*';
 debug = False;
+fileListaBlog="lista_blog.txt"
+listaLog = '../2015*_blogowp*';
 listaFile={};
 listaBlog=[];
 cond_404 = re.compile('.*HTTP\/1.[01]\" 40.*');
@@ -27,6 +27,10 @@ for log in glob.glob(listaLog):
 		if(cond_404.match(line)):
 			if(cond_galleria.match(line)):
 				blog = line.split('\"')[7];
+				try:
+					 type(conteggi[blog]);
+				except:
+					 conteggi[blog]={}; 
 				if debug: print line.split('\"')[9];
 				galleria = re.sub(r'GET /(galleria|galleriazoom)/(big/)?([\w\-!%]*)\b/?.*d* HTTP/\d\.\d', r'\g<3>', line.split('\"')[9]);
 				try : 
@@ -37,9 +41,11 @@ for log in glob.glob(listaLog):
 				try:
 					conteggi[blog][galleria]+=1;
 					if debug: print("num: "+str(conteggi[blog][galleria]));
+					#print "\""+galleria+"\",\""+str(conteggi[blog][galleria])+"\"\n";
 				except:
-					conteggi[blog]= dict(galleria = 1);
+					conteggi[blog][galleria] = 1;
 					if debug: print("num: "+str(conteggi[blog][galleria]));
+					#print "\""+galleria+"\",\""+str(conteggi[blog][galleria])+"\"\n";
 
 for name,fh in listaFile.items():
 	fh.close();
@@ -48,6 +54,7 @@ for name,fh in listaFile.items():
 for keys, values in conteggi.items():
 	file = open( "conteggi_"+keys, 'w');
 	for galleria, conteggio in values.items():
+		if debug: print "\""+galleria+"\",\""+str(conteggio)+"\"\n";
 		file.write("\""+galleria+"\",\""+str(conteggio)+"\"\n");
 	file.close();
 
